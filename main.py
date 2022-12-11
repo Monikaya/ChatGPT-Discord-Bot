a2589180823a223859830cbcfe683bd6f5cb2ab9
Monikaya <ahawker374@gmail.com>
2022-12-10 16:23:35 -0800

    it works now still not async

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import os
 import aiohttp
 import asyncio
 from dotenv import load_dotenv
-#from asyncChatGPT.asyncChatGPT import Chatbot
+# from asyncChatGPT.asyncChatGPT import Chatbot
 from revChatGPT.revChatGPT import Chatbot
 
+load_dotenv()
 intents = nextcord.Intents.default()
 intents.message_content = True
-load_dotenv()
+
 
 config = {"email": f"{os.getenv('CHATGPT_EMAIL')}", "password": f"{os.getenv('CHATGPT_PASS')}"}
-
 chatbot = Chatbot(config, conversation_id=None)
 
-bot = nextcord.Client()
+bot = nextcord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
 
 @bot.event
 async def on_message(message):
@@ -27,10 +28,8 @@ async def on_message(message):
         webhook = Webhook.from_url(f"{os.getenv('WEBHOOK')}", session=session)
         if message.webhook_id:
             return
-        print(f"Message:{message}")
-        print(f"Content: {message.content}")
-        #response = chatbot.get_chat_response(message.content, output="text")['message']
-        #await webhook.send(response)
+        response = chatbot.get_chat_response(message.content, output="text")['message']
+        await webhook.send(response)
 
 
 bot.run(os.getenv("TOKEN"))
